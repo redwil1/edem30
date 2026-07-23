2a5a7f75587ccc6ac698f30b8f5cb05ad45dc72e
unknown <maksimsrunin123>
2026-07-23 06:34:15 +0300

    Добавить реальные улицы для районных центров: Ахтубинск, Знаменск, Камызяк, Красный Яр, Нариманов

--- data/streetsByCity.ts
+++ data/streetsByCity.ts
@@ -1,9 +1,19 @@
 import { harabaliStreets } from "./harabaliStreets";
 import { astrakhanStreets } from "./astrakhanStreets";
+import { ahtubinskStreets } from "./ahtubinskStreets";
+import { znamenskStreets } from "./znamenskStreets";
+import { kamizyakStreets } from "./kamizyakStreets";
+import { krasnyyYarStreets } from "./krasnyyYarStreets";
+import { narimanovStreets } from "./narimanovStreets";
 
 export const streetsByCity: Record<string, string[]> = {
   Харабали: harabaliStreets,
   Астрахань: astrakhanStreets,
+  Ахтубинск: ahtubinskStreets,
+  Знаменск: znamenskStreets,
+  Камызяк: kamizyakStreets,
+  "Красный Яр": krasnyyYarStreets,
+  Нариманов: narimanovStreets,
 };
 
 export function getStreetsForCity(city: string | null): string[] {
